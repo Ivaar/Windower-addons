47f9e6275593c4cda596444e19e3a0c9bfbdbad8
Ivaar <sblms2j@yahoo.com>
2015-04-19 20:58:27 -0700

    Update ChatMan.lua

--- ChatMan/ChatMan.lua
+++ ChatMan/ChatMan.lua
@@ -37,64 +37,57 @@ On = '\31\204on\30\1'
 Off = '\31\167off\30\1'
 
 function check_setting(chat)
-	if settings[chat].sound and os.clock()-settings[chat].delay >= last_chat[chat] then
-		windower.play_sound(windower.addon_path .. '%s.wav':format(chat))
-		last_chat[chat] = os.clock()
-	end
+    if settings[chat].sound and os.clock()-settings[chat].delay >= last_chat[chat] then
+        windower.play_sound(windower.addon_path .. '%s.wav':format(chat))
+        last_chat[chat] = os.clock()
+    end
 end
 
 windower.register_event('incoming chunk', function(id,data)
-	if id == 0x017 then
-		local chat = packets.parse('incoming', data)
+    if id == 0x017 then
+        local chat = packets.parse('incoming', data)
         if chat['Mode'] == 12 or chat['gm'] then
-			check_setting('gm')
-		elseif chat['Mode'] == 3 and chat['Sender Name'] ~= windower.ffxi.get_player().name then
-			check_setting('tell')
-		elseif chat['Message']:lower():contains(windower.ffxi.get_player().name:lower()) and (chat['Mode'] == 0 or chat['Mode'] == 1 or chat['Mode'] == 4 or chat['Mode'] == 5 or chat['Mode'] == 26) then
-			check_setting('talk')
-		end
-	elseif id == 0x0DC then
-		check_setting('invite')
-	end
+            check_setting('gm')
+        elseif chat['Mode'] == 3 and chat['Sender Name'] ~= windower.ffxi.get_player().name then
+            check_setting('tell')
+        elseif chat['Message']:lower():contains(windower.ffxi.get_player().name:lower()) and (chat['Mode'] == 0 or chat['Mode'] == 1 or chat['Mode'] == 4 or chat['Mode'] == 5 or chat['Mode'] == 26) then
+            check_setting('talk')
+        end
+    elseif id == 0x0DC then
+        check_setting('invite')
+    end
 end)
 
 windower.register_event('emote', function(emote_id,sender_id,target_id)
-	if target_id == windower.ffxi.get_player().id and sender_id ~= windower.ffxi.get_player().id then
-		check_setting('emote') 
-	end
+    if target_id == windower.ffxi.get_player().id and sender_id ~= windower.ffxi.get_player().id then
+        check_setting('emote') 
+    end
 end)
 
 windower.register_event('examined', function(sender_name,sender_index)
     if sender_name ~= windower.ffxi.get_player().name then
-		check_setting('examine')
-	end
+        check_setting('examine')
+    end
 end)
 
 windower.register_event('addon command', function(command,arg1,arg2)
-	if settings[command] then
-		if tonumber(arg1) then
-			settings[command:lower()].delay = tonumber(arg1)
-		elseif tonumber(arg2) then
-			settings[command:lower()].delay = tonumber(arg2)
-		end
-		if arg1 and arg1 == 'on' or arg2 == 'on' then
-			settings[command:lower()].sound = true
-		elseif arg1 and arg1 == 'off' or arg2 == 'off' then
-			settings[command:lower()].sound = false
-		end
-		config.save(settings, 'all')
-	elseif command == 'save' then
-		settings:save()
-		print('Settings saved to %s':format(windower.ffxi.get_player().name))
-	else
-		print('valid commands: tell talk invite emote examine\ncommand arguments: [on] or [off] and or [replay delay(seconds)]\ne.g //cm tell on 5')
-	end
-	windower.add_to_chat(207, 'Sound/Replay delay [GM %s/%s] [Tell %s/%s] [Talk %s/%s] [Emote %s/%s] [Examine %s/%s] [Invite %s/%s]':format(settings.gm.sound and On or Off, settings.gm.delay, settings.tell.sound and On or Off, settings.tell.delay, settings.talk.sound and On or Off, settings.talk.delay, settings.emote.sound and On or Off, settings.emote.delay, settings.examine.sound and On or Off, settings.examine.delay, settings.invite.sound and On or Off, settings.invite.delay))
+    if settings[command] then
+        if tonumber(arg1) then
+            settings[command:lower()].delay = tonumber(arg1)
+        elseif tonumber(arg2) then
+            settings[command:lower()].delay = tonumber(arg2)
+        end
+        if arg1 and arg1 == 'on' or arg2 == 'on' then
+            settings[command:lower()].sound = true
+        elseif arg1 and arg1 == 'off' or arg2 == 'off' then
+            settings[command:lower()].sound = false
+        end
+        config.save(settings, 'all')
+    elseif command == 'save' then
+        settings:save()
+        print('Settings saved to %s':format(windower.ffxi.get_player().name))
+    else
+        print('valid commands: tell talk invite emote examine\ncommand arguments: [on] or [off] and or [replay delay(seconds)]\ne.g //cm tell on 5')
+    end
+    windower.add_to_chat(207, 'Sound/Replay delay [GM %s/%s] [Tell %s/%s] [Talk %s/%s] [Emote %s/%s] [Examine %s/%s] [Invite %s/%s]':format(settings.gm.sound and On or Off, settings.gm.delay, settings.tell.sound and On or Off, settings.tell.delay, settings.talk.sound and On or Off, settings.talk.delay, settings.emote.sound and On or Off, settings.emote.delay, settings.examine.sound and On or Off, settings.examine.delay, settings.invite.sound and On or Off, settings.invite.delay))
 end)
---[[
-elseif chat['Mode'] == 8 and chat['Sender Name'] ~= windower.ffxi.get_player().name then
-	print(chat['Sender Name'])
-	print('emote')
-elseif id ==  then
-	print('examine')
-end]]
