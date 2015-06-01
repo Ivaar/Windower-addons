970c4aea1fbd662e95d21ad3783322ec9dc1dd85
Ivaar <sblms2j@yahoo.com>
2015-05-31 17:51:41 -0700

    Update AuctionHelper.lua

--- AuctionHelper/AuctionHelper.lua
+++ AuctionHelper/AuctionHelper.lua
@@ -1,30 +1,3 @@
---[[
-Copyright © 2015, Ivaar
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of <addon name> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL <your name> BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
 require('lists')
 texts = require('texts')
 config = require('config')
@@ -32,27 +5,28 @@ packets = require('packets')
 res_items = require('resources').items
 res_zones = require('resources').zones
 
+
 windower.send_command('unload bidder')
 
 _addon.command = 'AH'
 _addon.name = 'AuctionHelper'
-_addon.version = '1.0.0.3'
+_addon.version = '1.5.31.15'
 _addon.author = 'Ivaar'
 
 default = {
-    text = {size = 10,font = 'Consolas',pos = {x = 390,y = 50}},
     auction_list = {
-        visibility=true,
+        display=true,
         timer=true,
         date=true,
         price=false,
         empty=true,
         slot=true,
     },
+    display = {text = {size = 10,font = 'Consolas'},pos = {x = 390,y = 50}}
 }
 
 settings = config.load(default)
-auction_list = texts.new(settings)
+auction_list = texts.new(settings.display)
 
 zones = {}
 zones.ah = L{'Bastok Mines', 'Bastok Markets', 'Norg', 'Southern San d\'Oria', 'Port San d\'Oria', 'Raboa', 'Windurst Woods', 'Windurst Walls', 'Kazham', 'Lower Jueno', 'Ru\'Lude Gardens', 'Port Jueno', 'Upper Jueno', 'Aht Urhgan Whitegate', 'Al Zahbi', 'Nashmau', 'Tavnazian Safehold', 'Western Adoulin', 'Eastern Adoulin'}
@@ -82,7 +56,7 @@ local display_box = function()
                 if (settings.auction_list.date) then
                     str = str..string.format(' [%s]',os.date('%c', timer))
                 end
-                str = str..string.format(' %s ',res_items[auction_box[x].item].en)
+                str = str..string.format(' %s ',auction_box[x].name)
                 if (auction_box[x].count ~= 1) then
                     str = str..string.format('x%d ',auction_box[x].count)
                 end
@@ -99,7 +73,7 @@ local display_box = function()
 end;
 
 windower.register_event('prerender', function()
-    if auction_box and settings.auction_list.visibility then
+    if auction_box and settings.auction_list.display then
         auction_list:text(display_box())
         auction_list:show()
     --else
@@ -107,7 +81,7 @@ windower.register_event('prerender', function()
     end
 end)
 
-windower.register_event('logout', function(name)
+windower.register_event('logout', function()
     auction_box = nil
     auction_list:hide()
 end)
@@ -115,7 +89,7 @@ end)
 windower.register_event('addon command', function(...)
     local commands = {...}
     local now = os.clock()
-    if zones.ah:contains(res_zones[windower.ffxi.get_info().zone].name) and (not lclock or lclock < now) then	
+    if not actions and zones.ah:contains(res_zones[windower.ffxi.get_info().zone].name) and (not lclock or lclock < now) then	
         if not commands[1] then
             lclock = now+1
             open_menu_command()
@@ -125,47 +99,39 @@ windower.register_event('addon command', function(...)
         if (commands[1] == 'buy' or commands[1] == 'sell') and commands[4] then
             if ah_proposal(commands[1],table.concat(commands, ' ',2,#commands-2):lower(),commands[#commands-1]:lower(),commands[#commands]) then lclock = now+3 end
         elseif commands[1] == 'clear' and commands[2] and commands[2]:lower() == 'sold' and auction_box then
-            lclock = now+1
-            for x=0,6 do
-                if auction_box[x] and(auction_box[x].status == 'Sold' or auction_box[x].status == 'Not Sold') then
-                    print(x)
-                    remove_sold(x)
-                    coroutine.sleep(1+math.random())
-                    lclock = lclock+1
-                end
-            end
+            remove_sold()
+            return
         end
     end
     if commands[1] == 'hide' then
         if #commands == 1 then
-            settings.auction_list.visibility = false
+            settings.auction_list.display = false
             auction_list:visible(false)
         elseif default.auction_list[commands[2]:lower()] ~= nil then
             settings.auction_list[commands[2]:lower()] = false
         end
-        return
     elseif commands[1] == 'show' then
         if #commands == 1 then
-            settings.auction_list.visibility = true
+            settings.auction_list.display = true
             auction_list:visible(true)
         elseif default.auction_list[commands[2]:lower()] ~= nil then
             settings.auction_list[commands[2]:lower()] = true
         end
-        return
     elseif commands[1] == 'save' then
         config.save(settings, 'all')
-        return
     elseif commands[1] == 'eval' then
         assert(loadstring(table.concat(commands, ' ',2)))()
-        return
     end
 end)
 
 windower.register_event('unhandled command', function(...)
     local commands = {...}
     commands[1] = commands[1]:lower()
-    if commands[1] ~= 'buy' and commands[1] ~= 'sell' and commands[1] ~= 'inbox' and commands[1] ~= 'outbox' and commands[1] ~= 'ibox' and commands[1] ~= 'obox' then return end
-    if not zones.ah:contains(res_zones[windower.ffxi.get_info().zone].name) then return end
+    if commands[1] ~= 'bazaar' and commands[1] ~= 'buy' and commands[1] ~= 'sell' and commands[1] ~= 'inbox' and commands[1] ~= 'outbox' and commands[1] ~= 'ibox' and commands[1] ~= 'obox' then return end
+    if commands[1] == 'bazaar' and #commands >= 3 then
+        bazaar_item(table.concat(commands, ' ',2,#commands-1):lower(),commands[#commands])
+    end
+    if actions or not zones.ah:contains(res_zones[windower.ffxi.get_info().zone].name) then return end
     local now = os.clock()
     if not lclock or lclock < now then
         if (commands[1] == 'outbox' or commands[1] == 'obox') then	
@@ -201,6 +167,7 @@ function update_sales_status(packet)
             auction_box[packet.Slot].price = packet['Price']
             auction_box[packet.Slot].item = packet['Item']
             auction_box[packet.Slot].count = packet['Count']
+            auction_box[packet.Slot].name = res_items[packet['Item']].en
             --auction_box[packet.Slot].category = packet['AH Category']
         end
     end
@@ -215,6 +182,36 @@ function find_empty_slot()
         end
     end
     return nil
+end
+
+function bazaar_item(item_name,price)
+    if not windower.ffxi.get_player() then return end
+    local item = get_item_res(item_name)
+    if not item then 
+        print('AH Error: %s not a valid item name.':format(item_name)) 
+        return false
+    end
+    if item.flags['No PC Trade'] == true or item.flags['Linkshell'] == true then 
+        print(item.flags)
+        return false
+    end
+    price = format_price(price)
+    if not price or price > 99999999 then print('AH Error: Invalid price.') return false end
+    actions = true
+    for ind,inv_item in ipairs(windower.ffxi.get_items(0)) do
+        if inv_item and inv_item.id == item.id and (inv_item.status == 0 or inv_item.status == 25) then
+            bazaar_set_price(ind,price)
+        end
+    end
+    actions = false
+end
+
+function bazaar_set_price(index,price)   
+    if bazaar then
+        local set_bazaar = packets.new('outgoing', 0x10A, {['Inventory Index'] = index,['Price'] = price})
+        packets.inject(set_bazaar)
+        coroutine.sleep(1+math.random())
+    end
 end
 
 windower.register_event('incoming chunk', function(id, original, modified, injected, blocked)
@@ -255,6 +252,15 @@ windower.register_event('incoming chunk', function(id, original, modified, injec
     end
 end)
 
+
+windower.register_event('outgoing chunk', function(id, original, modified, injected, blocked)
+    if id == 0x10B then
+        bazaar = true
+    elseif id == 0x109 then
+        bazaar = false
+    end
+end)
+
 function comma_value(n) -- credit http://richard.warburton.it
     local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
     return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
@@ -266,6 +272,7 @@ function space_hex(n)
 end
 
 function get_item_res(item)
+    item = windower.convert_auto_trans(item)
     for k,v in pairs(res_items) do
         if v.en:lower() == item:lower() or v.enl:lower() == item:lower() then
             return v
@@ -285,73 +292,92 @@ function find_item(item_id,item_count)
 end
 
 function open_menu_command()
-    if windower.ffxi.get_info().logged_in then
-        local o_menu = string.char(0x4C,0x1E,0,0,0x02,0,0x01,0)..string.char(0,0,0,0,0,0,0,0)
-        o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0,0,0,0,0)
-        o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0,0,0,0,0)
-        o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0)
-        --print('Modified packet 0x04C: %s\n%s bytes':format(space_hex(o_menu:hex()),#o_menu))
-        windower.packets.inject_incoming(0x4C,o_menu)
-    end
+    if not windower.ffxi.get_player() then return end
+    local o_menu = string.char(0x4C,0x1E,0,0,0x02,0,0x01,0)..string.char(0,0,0,0,0,0,0,0)
+    o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0,0,0,0,0)
+    o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0,0,0,0,0)
+    o_menu = o_menu..string.char(0,0,0,0,0,0,0,0)..string.char(0,0,0,0)
+    --print('Modified packet 0x04C: %s\n%s bytes':format(space_hex(o_menu:hex()),#o_menu))
+    windower.packets.inject_incoming(0x4C,o_menu)
 end
 
-function remove_sold(slot)
-    if auction_box and slot and auction_box[slot] and (auction_box[slot].status == 'Sold' or auction_box[slot].status == 'Not Sold') then
-        local i_sold = string.char(0x4E,0x1E,0,0,0x10,slot,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-        --print('Modified packet 0x04E: %s\n%s bytes':format(space_hex(i_sold:hex()),#i_sold))
-        windower.packets.inject_outgoing(0x4E,i_sold)
+function remove_sold()
+   if not windower.ffxi.get_player() then return end
+   actions = true
+   for slot=0,6 do
+       if auction_box and slot and auction_box[slot] and (auction_box[slot].status == 'Sold' or auction_box[slot].status == 'Not Sold') then
+            local i_sold = string.char(0x4E,0x1E,0,0,0x10,slot,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+            --print('Modified packet 0x04E: %s\n%s bytes':format(space_hex(i_sold:hex()),#i_sold))
+            windower.packets.inject_outgoing(0x4E,i_sold)
+
+            -- local i_sold = packets.new('outgoing', 0x4E, {['Type'] = 'Item sold',['Slot'] = slot})
+           -- print(packets.build(i_sold):hex())
+            coroutine.sleep(1+math.random())
+        end
     end
+    actions = false
 end
+
+function format_price(price)
+    price = price:gsub('%p', '')
+    if (not price) or
+      (string.match(price,'%a')) or
+      (not tonumber(price)) or
+      (tonumber(price) < 1) then
+        return nil
+    end
+    return tonumber(price)
+end
+
+function stack_conv()
+    local single
+    if (item.stack ~= 1) and (vol == '1' or vol == 'stack') then
+        single = 0
+    elseif vol == '0' or vol == 'single' then
+        single = 1
+    else print('AH Error: Specify Stack or Single') return false end
+end    
 
 function ah_proposal(bid, item_name, vol, price)
-    item_name = windower.convert_auto_trans(item_name)
+    if not windower.ffxi.get_player() then return end
     local item = get_item_res(item_name)
-    if not item then 
-        print('AHPack Error: %s not a valid item name.':format(item_name)) 
-        return false
-    end
+    if not item then print('AH Error: %s not a valid item name.':format(item_name)) return false end
     
-    if item.flags['No Auction'] == true then 
-        print(item.flags) 
-        return false
-    end
+    if item.flags['No Auction'] == true then print(item.flags) return false end
     
     local single
     if (item.stack ~= 1) and (vol == '1' or vol == 'stack') then
         single = 0
     elseif vol == '0' or vol == 'single' then
         single = 1
-    else print('AHPack Error: Specify Stack or Single') return false end
+    else print('AH Error: Specify Stack or Single') return false end
+    
     local trans = string.char(0x4E,0x1E,0,0)
     
-    price = price:gsub('%p', '')
-    if (not price) or
-      (string.match(price,'%a')) or
-      (not tonumber(price)) or
-      (tonumber(price) < 1) or
-      (bid == 'sell' and tonumber(price) > 999999999) or
-      (bid == 'buy' and tonumber(price) > windower.ffxi.get_items().gil) then
-        print('AH Error: Invalid price.')
-        return false
-    end
-    price = tonumber(price)
+    price = format_price(price)
     
-    if bid == 'buy' then
+    if bid == 'buy' and price and tonumber(price) <= windower.ffxi.get_items().gil then
         local slot = find_empty_slot() == nil and 0x07 or find_empty_slot()
         trans = trans..string.char(0x0E,slot,0,0, (price%256), (math.floor((price/256)%256)), (math.floor((price/65536)%256)), (math.floor((price/16777216)%256)), (item.id%256), (math.floor((item.id/256)%256)),0,0)
         --print('%s "%s" %s %s ID:%s':format(bid, item.en, comma_value(price),single == 1 and '[Single]' or '[Stack]',item.id))
-    elseif bid == 'sell' then
+    elseif bid == 'sell' and price and tonumber(price) <= 999999999 then
         if not auction_box then print('AH Error: Click auction counter or use /ah to initialize sales.') return	end
         if not find_empty_slot() then print('AHPack Error: No Empty Slots Available.') return end
         trans = trans.. string.char(0x04,0,0,0, (price%256), (math.floor((price/256)%256)), (math.floor((price/65536)%256)), (math.floor((price/16777216)%256)))
         local index = find_item(item.id, single == 1 and single or item.stack)
-        if not index then print('AHPack Error: %s of %s not found in inventory.':format(single == 1 and 'Single' or 'Stack',item.en)) return end
+        if not index then print('AH Error: %s of %s not found in inventory.':format(single == 1 and 'Single' or 'Stack',item.en)) return end
         trans = trans..string.char((index%256), (math.floor((index/256)%256)), (item.id%256), (math.floor((item.id/256)%256)))
         --print('%s "%s" %s %s ID:%s Ind:%s':format(bid, item.en, comma_value(price),single == 1 and '[Single]' or '[Stack]',item.id,index))
-    else return false end
+    else
+        print('AH Error: Invalid price.')
+        return false 
+    end
     
     trans = trans..string.char(single,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
     --print('Modified packet 0x04E: %s\n%s bytes':format(space_hex(trans:hex()),#trans))	
     windower.packets.inject_outgoing(0x4E,trans)
     return true
 end
+-----------------------------------------------------------------------------------------------------
+--print('AHPack buy "short or full item name" 0 10,000\n single OR 0 and stack OR 1.')
+-----------------------------------------------------------------------------------------------------
