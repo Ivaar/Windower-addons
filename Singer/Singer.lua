f57f07233462edabeb7f636dc8c256cdcd49e2de
Ivaar <sblms2j@yahoo.com>
2017-09-25 16:13:56 -0700

    Update Singer.lua

--- Singer/Singer.lua
+++ Singer/Singer.lua
@@ -1,7 +1,7 @@
 _addon.author = 'Ivaar'
 _addon.commands = {'Singer','sing'}
 _addon.name = 'Singer'
-_addon.version = '1.15.06.24'
+_addon.version = '1.17.09.25'
 require('luau')
 require('pack')
 packets = require('packets')
