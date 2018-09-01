619d29ff490f5c1ad0677d44d7736302b91512d9
Ivaar <7287342+Ivaar@users.noreply.github.com>
2018-08-31 19:07:02 -0700

    Create profiles.lua

---
+++ SellNPC/profiles.lua
@@ -0,0 +1,22 @@
+-- Add items to existing profiles or create your own to sell groups of items using alias commands
+local profiles = {}
+
+-- //sellnpc powder
+profiles['powder'] = S{
+    'prize powder',
+    }
+
+-- //sellnpc ore
+profiles['ore'] = S{
+    'iron ore',
+    'copper ore',
+    'tin ore',
+    }
+
+-- //sellnpc junk
+profiles['junk'] = S{
+    'chestnut',
+    'san d\'Or. carrot',
+    }
+
+return profiles
