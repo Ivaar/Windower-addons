ca82077f88b10dd8b8f33aff1c45e92cfc344576
Ivaar <7287342+Ivaar@users.noreply.github.com>
2018-09-02 13:22:29 -0700

    Revert SellNPC to v1.0.0.5

--- SellNPC/SellNPC.lua
+++ SellNPC/SellNPC.lua
@@ -1,13 +1,20 @@
 _addon.command = 'SellNPC'
-_addon.version = '2.0.0.0'
+_addon.version = '1.0.0.5'
 _addon.author = 'Ivaar'
 
 require('sets')
+config = require('config')
 profiles = require('profiles')
 res_items = require('resources').items
 
+default = {
+    delay = 1,
+    randomize_delay = true,
+    }
+
+settings = config.load(default)
+
 sales_que = {}
-selling = false
 
 function get_item_res(item)
     for k,v in pairs(res_items) do
@@ -33,25 +40,57 @@ function check_item(name)
     end
 end
 
-function incoming_shop_open(id, data)
-    if id == 0x03C then
-        if not selling then
-            selling = true
-            for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
-                if item and sales_que[item.id] and item.status == 0 then
-                    windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,item.count,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
-                    windower.packets.inject_outgoing(0x085,string.char(0x085,0x04,0,0,1,0,0,0))
-                end
+function sell_appraised_items()
+    for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
+        if item and appraised[item.id] and item.status == 0 then
+            windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,item.count,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
+            windower.packets.inject_outgoing(0x085,string.char(0x085,0x04,0,0,1,0,0,0))
+            if settings.delay > 0 then
+                coroutine.sleep(settings.delay + (settings.randomize_delay and math.random() or 0))
             end
-            sales_que = {}
-            selling = false
         end
     end
+    sales_que = {}
+    appraised = nil
+    requested = nil
+end
+
+function incoming_shop_packets(id, data)
+    if id == 0x03C then
+        appraised = {}
+        requested = {}
+        if not actions or coroutine.status(actions) == 'dead' then
+            for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
+                if item and sales_que[item.id] and not requested[item.id] and item.status == 0 then
+                    requested[item.id] = true
+                    windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,1,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
+                end
+            end
+        end
+    elseif id == 0x03D and appraised then
+        local item_id = windower.ffxi.get_items(0,data:byte(9)).id
+        if requested[item_id] and not appraised[item_id] then
+            appraised[item_id] = true
+            if not actions or coroutine.status(actions) == 'dead' then
+                actions = coroutine.schedule(sell_appraised_items, 1)
+            end
+        end
+    end
+end
+
+function reset()
+    coroutine.close(actions)
+    actions = nil
+    appraised = nil
+    requested = nil
 end
 
 function cmd(...)
     local commands = {...}
     if not commands[1] then
+    elseif commands[1] == 'delay' and tonumber(commands[2]) then
+        settings.delay = tonumber(commands[2])
+        settings:save('all')
     elseif profiles[commands[1]] then
         for name in pairs(profiles[commands[1]]) do
             check_item(name)
@@ -61,5 +100,6 @@ function cmd(...)
     end
 end
 
-windower.register_event('incoming chunk', incoming_shop_open)
+windower.register_event('incoming chunk', incoming_shop_packets)
+windower.register_event('zone change','logout','unload',  reset)
 windower.register_event('addon command', cmd)
