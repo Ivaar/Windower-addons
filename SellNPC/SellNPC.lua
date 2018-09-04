8b9b1ef4499d49df3db37978bf462fb199dc5af5
Ivaar <7287342+Ivaar@users.noreply.github.com>
2018-09-03 22:56:27 -0700

    SellNPC v2

--- SellNPC/SellNPC.lua
+++ SellNPC/SellNPC.lua
@@ -1,18 +1,11 @@
 _addon.command = 'SellNPC'
-_addon.version = '1.0.0.5'
+_addon.version = '2.0.0.0'
 _addon.author = 'Ivaar'
+_addon.name = 'SellNPC'
 
 require('sets')
-config = require('config')
 profiles = require('profiles')
 res_items = require('resources').items
-
-default = {
-    delay = 1,
-    randomize_delay = true,
-    }
-
-settings = config.load(default)
 
 sales_que = {}
 
@@ -25,81 +18,53 @@ function get_item_res(item)
     return nil
 end
 
-function check_item(name)
+function check_item(name, silent)
     local name = windower.convert_auto_trans(name):lower()
     local item = get_item_res(name)
     if not item then
-        print('Error: %s not a valid item name.':format(name))
-        return
-    end
-    if item.flags['No NPC Sale'] then
-        print('Error: Cannot sell %s to npc vendors':format(item.en))
+        windower.add_to_chat(207, '%s: "%s" not a valid item name.':format(_addon.name, name))
+    elseif item.flags['No NPC Sale'] then
+        windower.add_to_chat(207, '%s: Cannot sell "%s" to npc vendors.':format(_addon.name, item.en))
     else
         sales_que[item.id] = true
-        print('%s added to sales queue':format(item.en))
+        if silent then return end
+        windower.add_to_chat(207, '%s: "%s" added to sales queue.':format(_addon.name, item.en))
     end
 end
 
-function sell_appraised_items()
+function sell_all_items()
+    local num = 0
     for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
-        if item and appraised[item.id] and item.status == 0 then
+        if item and sales_que[item.id] and item.status == 0 then
             windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,item.count,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
             windower.packets.inject_outgoing(0x085,string.char(0x085,0x04,0,0,1,0,0,0))
-            if settings.delay > 0 then
-                coroutine.sleep(settings.delay + (settings.randomize_delay and math.random() or 0))
-            end
+            num = num + item.count
         end
     end
     sales_que = {}
-    appraised = nil
-    requested = nil
-end
-
-function incoming_shop_packets(id, data)
-    if id == 0x03C then
-        appraised = {}
-        requested = {}
-        if not actions or coroutine.status(actions) == 'dead' then
-            for index = 1, 80 do local item = windower.ffxi.get_items(0,index)
-                if item and sales_que[item.id] and not requested[item.id] and item.status == 0 then
-                    requested[item.id] = true
-                    windower.packets.inject_outgoing(0x084,string.char(0x084,0x06,0,0,1,0,0,0,item.id%256,math.floor(item.id/256)%256,index,0))
-                end
-            end
-        end
-    elseif id == 0x03D and appraised then
-        local item_id = windower.ffxi.get_items(0,data:byte(9)).id
-        if requested[item_id] and not appraised[item_id] then
-            appraised[item_id] = true
-            if not actions or coroutine.status(actions) == 'dead' then
-                actions = coroutine.schedule(sell_appraised_items, 1)
-            end
-        end
+    if num > 0 then
+        windower.add_to_chat(207, '%s: Selling %d items.':format(_addon.name, num))
     end
 end
 
-function reset()
-    coroutine.close(actions)
-    actions = nil
-    appraised = nil
-    requested = nil
+function initialize_shop(id, data)
+    if id == 0x03C then
+        sell_all_items()
+    end
 end
 
-function cmd(...)
+function sell_npc_command(...)
     local commands = {...}
     if not commands[1] then
-    elseif commands[1] == 'delay' and tonumber(commands[2]) then
-        settings.delay = tonumber(commands[2])
-        settings:save('all')
     elseif profiles[commands[1]] then
         for name in pairs(profiles[commands[1]]) do
-            check_item(name)
+            check_item(name, true)
         end
+        windower.add_to_chat(207, '%s: Loaded profile "%s"':format(_addon.name, commands[1]))
     else
         check_item(table.concat(commands,' '))
     end
 end
 
-windower.register_event('incoming chunk', incoming_shop_packets)
-windower.register_event('zone change','logout','unload',  reset)
-windower.register_event('addon command', cmd)
+windower.register_event('incoming chunk', initialize_shop)
+windower.register_event('addon command', sell_npc_command)
