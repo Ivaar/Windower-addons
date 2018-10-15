63641fcd46d4be2079aab7ed03c77fc62c8e24b8
Ivaar <7287342+Ivaar@users.noreply.github.com>
2018-10-14 17:32:16 -0700

    Update chococard.lua

--- chococard/chococard.lua
+++ chococard/chococard.lua
@@ -1,5 +1,6 @@
 require('luau')
 require('pack')
+bit = require('bit')
 extdata = require('extdata')
 
 _addon.name = 'chococard'
@@ -100,7 +101,7 @@ function decode_chocobo(type, str)
                 tab[i][k] = lookup(tt.fn, k, str:unpack(unpack(tt)))
             end
         end
-    end    
+    end
     return tab
 end
 
@@ -109,19 +110,7 @@ local chocobo_item = {
     card = S{2313,2339,2342,2402},
 }
 
-local log_chocobo_egg = function(str, item_id)
-    local tab = decode_chocobo('egg', str)
-    local tier = item_id > 2314 and item_id-2314 or item_id == 2314 and 2 or 1
-    windower.add_to_chat(207, '\nEgg T%d [%s warm]':format(tier, {'faintly','slightly','a bit','a little','somewhat'}[tier]))
-    if tab.is_bred then
-        windower.add_to_chat(207, 'Plan: %s DNA: [%s %s %s]':format(tab.plan, unpack(tab.DNA)))
-        windower.add_to_chat(207, 'Ability: %s unknowns: %d %d':format(tab.ability, tab.unknown1, tab.unknown2))
-        --windower.add_to_chat(207, v.extdata:hex())
-    end
-end
-
-local log_chocobo_card = function(str)
-    local tab = decode_chocobo('card', str)
+local log_chocobo = function(tab)
     if 'None' == tab.abilities[2] then tab.abilities[2] = nil end
     windower.add_to_chat(207, '\n%s (%s) [%s] Jockey: %s DNA: [%s %s %s]':format(tab.name, tab.gender, tab.color, tab.size, unpack(tab.DNA)))
     windower.add_to_chat(207, 'STR: %3d %s %d~%d/32RP %s':format(tab.STR.value, tab.STR.rank, tab.STR.rp, tab.STR.rp+1, tab.STR.legs and 'Legs and talons' or ''))
@@ -131,6 +120,7 @@ local log_chocobo_card = function(str)
     windower.add_to_chat(207, 'Abilities: %s':format(table.concat(tab.abilities, ' & ')))
     windower.add_to_chat(207, 'Personality: %s':format(tab.temperament))
     windower.add_to_chat(207, 'Weather: Prefers %s. Dislikes %s.':format(tab.weather.prefers, tab.weather.dislikes))
+
     if tab.unknown == 1 then
         windower.add_to_chat(207, 'Observed unknown value')
     end
@@ -145,6 +135,14 @@ windower.register_event('incoming chunk', function(id, data, modified, injected,
     end
 end)
 
+local get_speed = function(speed_mod, gear)
+    return math.min(speed_mod + gear, 8) / 8 * 20 + 80
+end
+
+local get_duration = function(time_mod, gear)
+    return (math.min(time_mod + 4, 11) + gear) * 4 + 1
+end
+
 windower.register_event('addon command', function(...)
     local commands = {...}
     commands[1] = commands[1] and commands[1]:lower()
@@ -152,9 +150,30 @@ windower.register_event('addon command', function(...)
         windower.add_to_chat(207, '--------------------------------------------------------------------------------')
         for i,v in ipairs(windower.ffxi.get_items(0)) do
             if chocobo_item.egg[v.id] then
-                log_chocobo_egg(v.extdata, v.id)
+                local tab = decode_chocobo('egg', v.extdata)
+                local tier = v.id > 2314 and v.id-2314 or v.id == 2314 and 2 or 1
+                windower.add_to_chat(207, '\nEgg T%d [%s warm]':format(tier, {'faintly','slightly','a bit','a little','somewhat'}[tier]))
+                if tab.is_bred then
+                    windower.add_to_chat(207, 'Plan: %s DNA: [%s %s %s]':format(tab.plan, unpack(tab.DNA)))
+                    windower.add_to_chat(207, 'Ability: %s unknowns: %d %d':format(tab.ability, tab.unknown1, tab.unknown2))
+                    --windower.add_to_chat(207, v.extdata:hex())
+                end
             elseif chocobo_item.card[v.id] then
-                log_chocobo_card(v.extdata)
+                local tab = decode_chocobo('card', v.extdata)
+                log_chocobo(tab)
+
+                local speed_mod = math.floor(tab.STR.value / 0x20) -- str rank
+                local time_mod = math.floor(tab.END.value / 0x20) -- end rank
+                for x = 1, 2 do
+                    if tab.abilities[x] == 'Gallop' then
+                        speed_mod = speed_mod + 1
+                    elseif tab.abilities[x] == 'Canter' then
+                        time_mod = time_mod + 1
+                    end
+                end
+
+                windower.add_to_chat(207, 'Riding Speed: %.1f%% (Purple Race Silks: %.1f%%)':format(get_speed(speed_mod, 0), get_speed(speed_mod, 1)))
+                windower.add_to_chat(207, 'Riding Time: %d Minutes (Red Race Silks: %d Minutes)':format(get_duration(time_mod, 0), get_duration(time_mod, 1)))
             end
         end
     elseif commands[1] == 'display' then
@@ -165,7 +184,7 @@ windower.register_event('addon command', function(...)
             [0x033] = windower.packets.last_incoming(0x033) or '',
             [0x034] = windower.packets.last_incoming(0x034) or '',
         }
-
+        
         if 70 ~= last[0x05B]:unpack('H', 0x10+1) then return end -- chocobo circuit zone id
 
         local option = last[0x05B]:unpack('H', 0x08+1)
@@ -174,21 +193,33 @@ windower.register_event('addon command', function(...)
         if menu_id == last[0x033]:unpack('H', 0x0C+1) then
             if option > 5 and option < 9 then
                 -- free runs and mission races
-                log_chocobo_card(windower.packets.last_incoming(0x05C):sub(0x04+1, 0x0B+1)..string.char(0):rep(16))
+                -- incoming packet 0x05C
+                -- 0x04 chocobo data
                 -- 0x0C orders
                 -- 0x10 equipment
                 -- 0x14 affiliation
                 -- 0x1C
+                local tab = decode_chocobo('card', windower.packets.last_incoming(0x05C):sub(0x04+1, 0x0B+1)..string.char(0):rep(16))
+                if option > 6 then
+                    tab.name = windower.packets.last_incoming(0x05D):sub(0x38+1, 0x47+1)
+                else
+                    tab.name = last[0x033]:sub(0x10+1, 0x1F+1)
+                end
+                log_chocobo(tab)
             end
-        elseif menu_id == last[0x034]:unpack('H', 0x2C+1) then
+        elseif menu_id == last[0x034]:unpack('H', 0x2C+1) and bit.band(option, 0x07) == 0 then
             -- crystal stakes padock
+            -- incoming packet 0x05C 
             -- 0x04
             -- 0x08 affiliation
             -- 0x0C orders
             -- 0x10 equipment
-            log_chocobo_card(windower.packets.last_incoming(0x05C):sub(0x14+1, 0x1B+1)..string.char(0):rep(16))
+            -- 0x14 chocobo data
             -- 0x1C saddle
-            -- 0x20 chocobo index
+            -- 0x20
+            local tab = decode_chocobo('card', windower.packets.last_incoming(0x05C):sub(0x14+1, 0x1B+1)..string.char(0):rep(16))
+            tab.name = windower.packets.last_incoming(0x05D):sub(0x28+1, 0x37+1)
+            log_chocobo(tab)
         end
     end
 end)
