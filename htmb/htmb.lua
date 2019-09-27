1dc033098e4a609736ae0452e2775bcb4ed3087d
Ivaar <7287342+Ivaar@users.noreply.github.com>
2019-09-27 15:42:27 -0700

    Skillchains: pet abilities fixed

--- htmb/htmb.lua
+++ htmb/htmb.lua
@@ -7,6 +7,9 @@ require('luau')
 require('pack')
 bit = require('bit')
 
+local state = 0
+local last_attempt = os.time()
+
 buy_list = {10,2,5,9,3} -- key items will be purchased in this order until you are unable to buy more
 
 htmb_map = {
@@ -43,58 +46,60 @@ htmb_npcs = {
     [240] = {name = 'Mimble-Pimble',  menu_id = 895}, -- Port Windurst (L-5)
 }
 
-menu_options = 0
-merit_points = 0
-
 math.has_bit = function(mask, offset)
     return math.floor(mask/2^offset)%2 == 1
 end
 
-function get_option_index()
+function get_option_index(menu_options, merit_points)
     for x = 1, #buy_list do
         local option = buy_list[x]
         if htmb_map[option] and htmb_map[option].cost <= merit_points and menu_options:has_bit(option) then
-            return option
+            return 0x100 * option + 0x02
         end
     end
+    return 0x40000000
 end
 
 function initiate_npc(name)
-    local self = windower.ffxi.get_mob_by_target('me')
     local target = windower.ffxi.get_mob_by_name(name)
-    if not self or self.status > 0 then return end
     if target and math.sqrt(target.distance) < 6 and target.valid_target and target.is_npc and bit.band(target.spawn_type, 0xDF) == 2 then
         windower.packets.inject_outgoing(0x1A, 'I2H2d2':pack(0xE1A,target.id,target.index,0,0,0))
     end
+    state = 1
+end
+
+local function inject_option(npc_id, npc_index, zone_id, menu_id, option_index, bool)
+    windower.packets.inject_outgoing(0x5B, 'I3H4':pack(0, npc_id, option_index, npc_index, bool, zone_id, menu_id))
+    state = 2
 end
 
 windower.register_event('incoming chunk', function(id, data, modified, injected, blocked)
-    if id == 0x034 then
-        local zone_id, menu_id = data:unpack('H2', 43)
+    if id == 0x034 and state == 1 and os.time()-last_attempt < 3 then
+        local zone_id, menu_id = data:unpack('H2', 0x2A+1)
         if htmb_npcs[zone_id] and menu_id == htmb_npcs[zone_id].menu_id then
-            menu_options, merit_points = data:unpack('I2', 13)
-            windower.send_command('wait 2;setkey escape;wait .5;setkey escape up;')
+            inject_option(data:unpack('I', 0x04+1), data:unpack('H', 0x28+1), zone_id, menu_id, get_option_index(data:unpack('I2', 13)), 0)
+            return true
         end
     end
 end)
 
 windower.register_event('outgoing chunk', function(id, data, modified, injected, blocked)
-    if id == 0x05B then
+    if id == 0x05B and state ~= 0 and (data:byte(15) == 0 or not injected)  then
+        state = 0
         local zone_id, menu_id = data:unpack('H2', 17)
-        if htmb_npcs[zone_id] and menu_id == htmb_npcs[zone_id].menu_id and data:byte(15) == 0 then
-            local new_option = get_option_index()
-            if data:unpack('I', 9) == 0x40000000 and new_option then
-                initiate_npc(htmb_npcs[zone_id].name)
-                return data:sub(1,8)..string.char(0x02,new_option,0,0)..data:sub(13)
-            end
+        if htmb_npcs[zone_id] and menu_id == htmb_npcs[zone_id].menu_id and data:unpack('I', 9) ~= 0x40000000 then
+            initiate_npc(htmb_npcs[zone_id].name)
         end
     end
 end)
 
 windower.register_event('addon command', function()
     local zone = windower.ffxi.get_info().zone
-
-    if htmb_npcs[zone] then
-        initiate_npc(htmb_npcs[zone].name)
+    local player = windower.ffxi.get_player()
+    if not player or not htmb_npcs[zone] then
+        return
+    elseif state == 1 and player.status == 0 and os.time()-last_attempt > 3 then
+        state = 0
     end
+    initiate_npc(htmb_npcs[zone].name)
 end)
